9a87f765003be563be9eab97f84f83c24c666f18
Andrey Galkin <andrey@futoin.org>
2019-11-04 03:53:09 +0200

    NEW: initial folder structure

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var fs = require( 'fs' );
+
+module.exports = function( grunt ) {
+    grunt.initConfig( {
+        pkg: grunt.file.readJSON( 'package.json' ),
+
+        eslint: {
+            options: {
+                fix: true,
+                ignore: false,
+            },
+            target: [
+                '*.js',
+                'lib/**/*.js',
+                'test/**/*.js',
+            ],
+        },
+        jsdoc2md: {
+            README: {
+                src: [ '*.js', 'lib/**/*.js' ],
+                dest: "README.md",
+                options: { template: fs.readFileSync( 'misc/README.hbs', 'utf8' ) },
+            },
+        },
+        replace: {
+            README: {
+                src: "README.md",
+                overwrite: true,
+                replacements: [
+                    {
+                        from: "$$pkg.version$$",
+                        to: "<%= pkg.version %>",
+                    },
+                ],
+            },
+        },
+        nyc: {
+            cover: {
+                options: {
+                    cwd: '.',
+                    exclude: [
+                        'coverage/**',
+                        'dist/**',
+                        'es5/**',
+                        'examples/**',
+                        'test/**',
+                        '.eslintrc.js',
+                        'Gruntfile.js',
+                        'webpack.*.js',
+                    ],
+                    reporter: [ 'lcov', 'text-summary' ],
+                    reportDir: 'coverage',
+                    all: true,
+                },
+                cmd: false,
+                args: [ 'mocha', 'test/*test.js' ],
+            },
+            report: {
+                options: {
+                    reporter: 'text-summary',
+                },
+            },
+        },
+    } );
+
+    grunt.loadNpmTasks( 'grunt-eslint' );
+    grunt.loadNpmTasks( 'grunt-simple-nyc' );
+
+    grunt.registerTask( 'check', [ 'eslint' ] );
+
+    grunt.registerTask( 'node', [ 'nyc' ] );
+    grunt.registerTask( 'test', [
+        'check',
+        'node',
+        'doc',
+    ] );
+
+    grunt.loadNpmTasks( 'grunt-jsdoc-to-markdown' );
+    grunt.loadNpmTasks( 'grunt-text-replace' );
+    grunt.registerTask( 'doc', [ 'jsdoc2md:README', 'replace:README' ] );
+
+    grunt.registerTask( 'default', [ 'check' ] );
+};
